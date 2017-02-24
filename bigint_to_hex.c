ce9808f786006b58089e877e458d4825b095b383
Mara Bos <m-ou.se@m-ou.se>
2017-02-24 18:58:41 +0100

    Fix bigint_to_hex for 0.

--- bigint_to_hex.c
+++ bigint_to_hex.c
@@ -4,6 +4,13 @@ size_t bigint_to_hex(
 	digit_t const *digits, sdigit_t ssize,
 	char *buf, size_t buflen
 ) {
+	if (ssize == 0) {
+		if (buflen >= 2) {
+			*buf++ = '0';
+			*buf++ = '\0';
+		}
+		return 2;
+	}
 	char const *hex = "0123456789ABCDEF";
 	size_t needed = ABS(ssize) * sizeof(digit_t) * CHAR_BIT / 4 + 1 + (ssize < 0);
 	if (needed < buflen) {
