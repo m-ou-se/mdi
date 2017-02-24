22af8a14dd2aebb76e8a3d498071d072bb3ae819
Mara Bos <m-ou.se@m-ou.se>
2017-02-24 17:12:54 +0100

    Fix formatting negative numbers.

--- bigint.c
+++ bigint.c
@@ -174,8 +174,9 @@ size_t bigint_to_hex(
 	char *buf, size_t buflen
 ) {
 	char const *hex = "0123456789ABCDEF";
-	size_t needed = ABS(ssize) * sizeof(digit_t) * CHAR_BIT / 4 + 1;
+	size_t needed = ABS(ssize) * sizeof(digit_t) * CHAR_BIT / 4 + 1 + (ssize < 0);
 	if (needed < buflen) {
+		if (ssize < 0) *buf++ = '-';
 		for (size_t i = ABS(ssize); i--; ) {
 			int shift = sizeof(digit_t) * CHAR_BIT;
 			while (shift) {
