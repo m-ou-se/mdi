584f18d03d6142de032d124cc5be5d376f7d6fd4
Mara Bos <m-ou.se@m-ou.se>
2017-02-24 18:58:34 +0100

    Fix bigint_alloc_add.

--- bigint_alloc_add.c
+++ bigint_alloc_add.c
@@ -1,11 +1,5 @@
 #include "bigint_impl.h"
 
-static void strip_leading_zeros(struct bigint *b) {
-	size_t n = ABS(b->ssize);
-	while (n && b->digits[n - 1] == 0) --n;
-	b->ssize = b->ssize < 0 ? -(sdigit_t)n : n;
-}
-
 struct bigint *bigint_alloc_add(
 	digit_t const *a, sdigit_t assize,
 	digit_t const *b, sdigit_t bssize
@@ -13,9 +7,13 @@ struct bigint *bigint_alloc_add(
 	struct bigint *r;
 	if ((assize < 0) == (bssize < 0)) {
 		// Same signs, so add.
-		r = bigint_alloc(assize + (assize < 0 ? -1 : 1));
-		mdi_add(r->digits, ABS(r->ssize), a, ABS(assize), b, ABS(bssize));
-		strip_leading_zeros(r);
+		sdigit_t rn = MAX(ABS(assize), ABS(bssize)) + 1;
+		r = bigint_alloc(assize < 0 ? -rn : rn);
+		mdi_add(r->digits, rn, a, ABS(assize), b, ABS(bssize));
+		if (r->digits[rn - 1] == 0) {
+			// Strip leading zero.
+			r->ssize -= r->ssize < 0 ? -1 : 1;
+		}
 	} else {
 		// Different signs, so subtract.
 		sdigit_t rssize = mdi_sub(NULL, 0, a, ABS(assize), b, ABS(bssize));
--- bigint_impl.h
+++ bigint_impl.h
@@ -2,3 +2,4 @@
 #include "bigint.h"
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
