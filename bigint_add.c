091e9ad382a351c031fcc61a250fb005d5292895
Mara Bos <m-ou.se@m-ou.se>
2017-02-24 22:47:44 +0100

    Fix implementation of bigint add.

--- bigint_add.c
+++ bigint_add.c
@@ -1,20 +1,38 @@
+#include <assert.h>
+
 #include "bigint_impl.h"
 
 struct bigint *bigint_add(
 	struct bigint *a,
 	digit_t const *b, sdigit_t bssize
 ) {
+	sdigit_t const an = ABS(a->ssize);
+	sdigit_t const bn = ABS(bssize);
 	if ((a->ssize < 0) == (bssize < 0)) {
 		// Same signs, so add.
-		if (mdi_add(a->digits, ABS(a->ssize), a->digits, ABS(a->ssize), b, ABS(bssize))) {
+		bool const potential_carry =
+			an == bn && (a->digits[an - 1] + b[an - 1] + 1 <= b[an - 1]);
+		sdigit_t const rn = MAX(an, bn) + potential_carry;
+		if (bn > an) a = bigint_resize(a, a->ssize < 0 ? -rn : rn);
+		if (mdi_add(a->digits, ABS(a->ssize), a->digits, an, b, bn)) {
 			// Carry
-			a = bigint_resize(a, a->ssize + (a->ssize < 0 ? -1 : 1));
-			a->digits[ABS(a->ssize) - 1] = 1;
+			assert(potential_carry);
+			assert(rn == an + 1);
+			a = bigint_resize(a, rn);
+			a->digits[rn - 1] = 1;
+		} else if (ABS(a->ssize) == rn) {
+			// No carry, potentially allocated one digit too many.
+			if (a->digits[rn - 1] == 0) {
+				// Strip leading zero.
+				a->ssize -= a->ssize < 0 ? -1 : 1;
+			}
 		}
 	} else {
 		// Different signs, so subtract.
-		sdigit_t rssize = mdi_sub(a->digits, ABS(a->ssize), a->digits, ABS(a->ssize), b, ABS(bssize));
-		a->ssize = a->ssize < 0 ? -rssize : rssize;
+		sdigit_t rssize = mdi_sub(NULL, 0, a->digits, an, b, bn);
+		if (a->ssize < 0) rssize = -rssize;
+		a = bigint_resize(a, rssize);
+		if (rssize != 0) mdi_sub(a->digits, ABS(a->ssize), a->digits, an, b, bn);
 	}
 	return a;
 }
--- bigint_alloc_add.c
+++ bigint_alloc_add.c
@@ -4,22 +4,26 @@ struct bigint *bigint_alloc_add(
 	digit_t const *a, sdigit_t assize,
 	digit_t const *b, sdigit_t bssize
 ) {
+	sdigit_t const an = ABS(assize);
+	sdigit_t const bn = ABS(bssize);
 	struct bigint *r;
 	if ((assize < 0) == (bssize < 0)) {
 		// Same signs, so add.
-		sdigit_t rn = MAX(ABS(assize), ABS(bssize)) + 1;
+		bool const potential_carry =
+			an == bn && (a[an - 1] + b[an - 1] + 1 <= b[an - 1]);
+		sdigit_t rn = MAX(an, bn) + potential_carry;
 		r = bigint_alloc(assize < 0 ? -rn : rn);
-		mdi_add(r->digits, rn, a, ABS(assize), b, ABS(bssize));
+		mdi_add(r->digits, rn, a, an, b, bn);
 		if (r->digits[rn - 1] == 0) {
 			// Strip leading zero.
 			r->ssize -= r->ssize < 0 ? -1 : 1;
 		}
 	} else {
 		// Different signs, so subtract.
-		sdigit_t rssize = mdi_sub(NULL, 0, a, ABS(assize), b, ABS(bssize));
+		sdigit_t rssize = mdi_sub(NULL, 0, a, an, b, bn);
 		if (assize < 0) rssize = -rssize;
 		r = bigint_alloc(rssize);
-		if (r->ssize) mdi_sub(r->digits, ABS(r->ssize), a, ABS(assize), b, ABS(bssize));
+		if (r->ssize) mdi_sub(r->digits, ABS(r->ssize), a, an, b, bn);
 	}
 	return r;
 }
