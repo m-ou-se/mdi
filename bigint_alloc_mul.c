7ee75ebcdfd075f50b89929d2f95ad3fcf5d87e3
Mara Bos <m-ou.se@m-ou.se>
2017-04-15 11:54:50 +0200

    Make bigint_alloc_mul work for negative numbers.

--- bigint_alloc_mul.c
+++ bigint_alloc_mul.c
@@ -5,8 +5,11 @@ struct bigint *bigint_alloc_mul(
 	digit_t const *b, sdigit_t bssize
 ) {
 	if (assize == 0 || bssize == 0) return bigint_alloc(0);
-	size_t rn = ABS(assize) + ABS(bssize);
-	struct bigint *r = bigint_alloc((assize < 0) != (bssize < 0) ? -(sdigit_t)rn : rn);
+	sdigit_t rn = ABS(assize) + ABS(bssize);
+	struct bigint *r = bigint_alloc(rn);
 	mdi_mul(r->digits, rn, a, ABS(assize), b, ABS(bssize));
+	while (rn--) if (r->digits[rn] != 0) break;
+	++rn;
+	r->ssize = (assize < 0) != (bssize < 0) ? -rn : rn;
 	return r;
 }
